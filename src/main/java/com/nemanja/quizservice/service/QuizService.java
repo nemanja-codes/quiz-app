126c08d012146cb834355996f85d16764b5ada81
Nemanja Markovic <necamark@gmail.com>
2023-10-13 23:44:00 +0200

    Zavrsene metode u Quiz Service klasi

--- src/main/java/com/nemanja/quizservice/service/QuizService.java
+++ src/main/java/com/nemanja/quizservice/service/QuizService.java
@@ -1,18 +1,17 @@
 package com.nemanja.quizservice.service;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-//import com.nemanja.quizservice.dao.QuestionDao;
+
 import com.nemanja.quizservice.dao.QuizDao;
 import com.nemanja.quizservice.feign.QuizInterface;
-//import com.nemanja.quizservice.model.Question;
 import com.nemanja.quizservice.model.QuestionWrapper;
 import com.nemanja.quizservice.model.Quiz;
 import com.nemanja.quizservice.model.Response;
@@ -39,27 +38,15 @@ public class QuizService {
 	}
 
 	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-		
-		  Quiz quiz = quizDao.findById(id).get();
-		  List<Integer> questionIds = quiz.getQuestionIds();
-		  
-		  ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
-		  
-		  
+		Quiz quiz = quizDao.findById(id).get();
+		List<Integer> questionIds = quiz.getQuestionIds();  
+		ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
 		return questions;
 	}
 
 	public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-		/*
-		 * Quiz quiz = quizDao.findById(id).get(); List<Question> questions =
-		 * quiz.getQuestions();
-		 */
-		int right = 0;
-		/*
-		 * int i = 0; for(Response r : responses) {
-		 * if(r.getResponse().equals(questions.get(i).getRightAnswer())) right++; i++; }
-		 */
-		return new ResponseEntity<>(right, HttpStatus.OK);
+		ResponseEntity<Integer> score = quizInterface.getScore(responses);
+		return score;
 	}
 	
 	
